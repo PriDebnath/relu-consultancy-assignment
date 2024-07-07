e6b4e4c193dd47aae4b4e7106f153c3c28641581
Pritam <debnathpritam0802@gmail.com>
2024-07-07 20:36:56 +0530

    auth service

---
+++ src/app/module/auth/services/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { User } from 'src/app/module/auth/models/user.model';
+// import { environment } from 'src/environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private API_URL: string = '';
+  // private API_URL: string = environment.APIEndpoint;
+
+  constructor(private http: HttpClient) {}
+
+  login(data: User) {
+    return this.http.post<User>(this.API_URL + 'accounts/auth/login/', data);
+  }
+
+  register(data: User) {
+    return this.http.post<User>(this.API_URL + 'accounts/registration/', data);
+  }
+
+  refreshToken(token: string) {
+    return this.http.post<User>(this.API_URL + 'auth/refresh/', { token });
+  }
+
+  getUser(data?: { user_id?: number }) {
+    return this.http.get<User>(
+      this.API_URL + 'accounts/users/' + data?.user_id! + '/'
+    );
+  }
+}
