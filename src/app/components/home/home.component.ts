e9c44a968a99827e3a497a3938bca93c6797cd17
Pritam <debnathpritam0802@gmail.com>
2024-07-08 19:40:53 +0530

    right bar done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,53 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   sidebarLinks: string[] = ['Home'];
-  postSrc: string[] = ['../../../assets/images/post-1.png'];
   currentSidebarLink: string = 'home';
+
+  posts = [
+    {
+      postSrc: '../../../assets/images/post-1.png',
+      personSrc: '../../../assets/images/person-ll.png',
+      personName: 'Lara Leones',
+      personUserName: '@thewallart',
+    },
+    {
+      postSrc: '../../../assets/images/post-2.png',
+      personSrc: '../../../assets/images/person-tj.png',
+      personName: 'Thomas J.',
+      personUserName: '@thecustomcreater',
+    },
+  ];
+
+  artists = [
+    {
+      postSrc: '../../../assets/images/artist-post-1.png',
+      personSrc: '../../../assets/images/artist-1.png',
+      personName: 'Thomas Edward',
+      personUserName: '@thewildwithyou',
+    },
+
+    {
+      postSrc: '../../../assets/images/artist-post-2.png',
+      personSrc: '../../../assets/images/artist-2.png',
+      personName: 'Chris Doe',
+      personUserName: '@thewildwithyou',
+    },
+    {
+      postSrc: '../../../assets/images/artist-post-3.png',
+      personSrc: '../../../assets/images/artist-3.png',
+      personName: 'Emilie Jones',
+      personUserName: '@thewildwithyou',
+    },
+    {
+      postSrc: '../../../assets/images/artist-post-4.png',
+      personSrc: '../../../assets/images/artist-4.png',
+      personName: 'Jessica Williams',
+      personUserName: '@thewildwithyou',
+    },
+  ];
+
   constructor() {}
+
   handleCurrentSidebarLink(link: string) {
     this.currentSidebarLink = link;
   }
