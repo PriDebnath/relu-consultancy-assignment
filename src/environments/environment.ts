b1fbb0535191112dca350090fb46e24552b03ab5
Pritam <debnathpritam0802@gmail.com>
2024-07-09 15:35:26 +0530

    installed firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAGpbGGA63DSon1egPCw3KC86uwGpFTwM0',
+    authDomain: 'relu-consultancy-assignm-40129.firebaseapp.com',
+    projectId: 'relu-consultancy-assignm-40129',
+    storageBucket: 'relu-consultancy-assignm-40129.appspot.com',
+    messagingSenderId: '31478385302',
+    appId: '1:31478385302:web:5bb69a44b42c32716a50b7',
+    measurementId: 'G-XLWPN6MJSK',
+  },
+};
